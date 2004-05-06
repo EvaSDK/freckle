3be18ae16afe8cd686d3b72fc86fcc79662eb41e
Gilles Dartiguelongue <dartigug@esiee.fr>
2004-05-06 16:42:23 +0000

    conneries de rémi --

--- tools.php
+++ tools.php
@@ -38,11 +38,11 @@
  <img src="essai-freckle.png" title="bann" alt="logo" />
  </div>
 
-  <div class="menu">
+ <div class="menu">
   <ul>
   <li><h2>Général</h2>
   <ul>
-   <li><a href="index2.php">Accueil</a></li>
+   <li><a href="index.php">Accueil</a></li>
    <li><a href="documents.php">Documents</a></li>
    <li><a href="display.php?what=site">Autres sites ESIEE</a></li>
    <li><a href="tools.php">Outils et recommandations</a></li>
@@ -66,7 +66,7 @@
    <li><a href="http://piartt.free.fr">Mainteneur Précédent</a></li>
   </ul></li>
   </ul>
-  </div>
+ </div>
 
  <div class="content">
 
