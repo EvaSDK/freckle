565b9070f7513d9ed4129e451a773696e8b3da3e
Gilles Dartiguelongue <dartigug@esiee.fr>
2004-10-24 10:05:39 +0000

    correction essai backend sql

--- inc/backend_sql.php
+++ inc/backend_sql.php
@@ -4,7 +4,8 @@ $SQL = "pgSQL";
 
 function q_num_rows( $o1 )
 {
-	switch ( global $SQL )
+	global $SQL;
+	switch ( $SQL )
 	{
 		case "mySQL": $result = mysql_num_rows($o1); break;
 		case "pgSQL": $result =    pg_num_rows($o2); break;
