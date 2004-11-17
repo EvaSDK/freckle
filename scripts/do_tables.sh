9c93a49932108eec7ef25701c8926bd677df48af
Gilles Dartiguelongue <dartigug@esiee.fr>
2004-11-17 18:29:28 +0000

    correction du script de génération des tables sql

--- scripts/do_tables.sh
+++ scripts/do_tables.sh
@@ -2,7 +2,7 @@
 # Script de génération des tables de fichiers et de reference
 
 REPS="EL IN PH MA EN"
-BASE="/home/web/htdocs/freckle/files"
+BASE="/home/eva/web/htdocs/freckle/files/"
 cpt=0;
 
 cd $BASE
@@ -17,7 +17,7 @@ do
   for x in ${FILES}
   do
     cpt=$(echo $[$cpt+1]);
-    file=$(echo $x | sed "s/\"//g;s/^/\'$y\//;s/$/\'/")
+    file=$(echo $x | sed "s/\"//g;s/^/\"$y\//;s/$/\"/")
     echo "INSERT INTO fichiers (url,annee_prod,commentaire) VALUES ($file,0,'');" >> ${BASE}/generated.sql
     
     #matière
@@ -29,13 +29,11 @@ do
     "EN") id_cat=10;;
     esac
 
-    echo "INSERT INTO reference (id_categorie,id_fichier,id_type) VALUES ($id_cat,$cpt,0);" >> ${BASE}/reference.sql 
-
     #année
     annee=$(echo $file | egrep -o "[-|.]+[0-9xX]{1,3}[-|.]+" | egrep -o "[0-9]+" | sed "s/..$//" )
     for z in ${annee}
     do
-      echo "INSERT INTO reference (id_categorie,id_fichier,id_type) VALUES ($z,$cpt,0);" >> ${BASE}/reference.sql
+      echo "INSERT INTO reference (id_categorie1,id_categorie2,id_fichier,id_type) VALUES ($z,$id_cat,$cpt,0);" >> ${BASE}/reference.sql
     done
     
   done
