700cffd6d84837f1d4a04151103190c554a6d028
Gilles Dartiguelongue <dartigug@esiee.fr>
2006-06-18 05:20:12 +0000

    correction du comportement de vfs_handling qui était complétement cassé

--- inc/management.inc.php
+++ inc/management.inc.php
@@ -117,7 +117,7 @@ function display_list_entries($what,$offset)
 				preg_match( $format, $filename, $result );
 				echo "\t<td>".$result[1]."</td>\n";
 				echo "\t<td>".$result[2]."</td>\n";
-				echo "\t<td><a href=\"".$arr."\" title='".$v["commentaire"]."'>".$result[3]."</a></td>\n";
+				echo "\t<td><a href=\"".$arr["url"]."\" title='".$v["commentaire"]."'>".$result[3]."</a></td>\n";
 				break;
 			case "defect":
 				echo "\t<td><input type='checkbox' name='ids-$id' value='$id'/>\n";
@@ -269,6 +269,7 @@ function get_form($what)
 function vfs_handling( $arr )
 {
 	global $repos_html;
+	$arr = array( "url" => $arr );
 	$scheme = substr( $arr["url"], 0, strpos( $arr["url"], ":") );
 
 	switch( $scheme )
